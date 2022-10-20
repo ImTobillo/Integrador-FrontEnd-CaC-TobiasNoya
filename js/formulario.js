36c04ead2e2df9c3089b9d2f577a8ca32fbe9b3c
tobias <tobinoyacabj@gmail.com>
2022-10-20 15:24:56 -0300

    formularioJShecho

---
+++ js/formulario.js
@@ -0,0 +1,25 @@
+const formulario = document.getElementById("formularioEntradas");
+const spanTotalAPagar = document.getElementById("total");
+
+function calcular(e)
+{
+    e.preventDefault();
+
+    let categoria = document.getElementById("categoria");
+    let cantidad = document.getElementById("cantidad");
+    let totalAPagar;
+
+    if (categoria.value = "estudiante")
+        totalAPagar = (200 - 200 * 0.8) * cantidad;
+    else if (categoria.value = "trainee")
+        totalAPagar = (200 - 200 * 0.5) * cantidad;
+    else
+        totalAPagar = (200 - 200 * 0.15) * cantidad;
+
+    spanTotalAPagar.innerHTML = totalAPagar;
+}
+
+formulario.onsubmit = calcular;
+
+
+
