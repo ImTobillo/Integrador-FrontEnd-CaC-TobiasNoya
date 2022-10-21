655113e754fb1a17f6cf63e0e1ed8e472be6e542
tobias <tobinoyacabj@gmail.com>
2022-10-21 14:01:32 -0300

    terminadoJS

--- js/formulario.js
+++ js/formulario.js
@@ -5,13 +5,13 @@ function calcular(e)
 {
     e.preventDefault();
 
-    let categoria = document.getElementById("categoria");
-    let cantidad = document.getElementById("cantidad");
+    let categoria = document.getElementById("categoria").value;
+    let cantidad = document.getElementById("cantidad").value;
     let totalAPagar;
 
-    if (categoria.value = "estudiante")
+    if (categoria === "estudiante")
         totalAPagar = (200 - 200 * 0.8) * cantidad;
-    else if (categoria.value = "trainee")
+    else if (categoria === "trainee")
         totalAPagar = (200 - 200 * 0.5) * cantidad;
     else
         totalAPagar = (200 - 200 * 0.15) * cantidad;
